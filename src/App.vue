6bbe6b83e21a152cf6df790548706881ae779a85
Jenny Vargas <jennypatriciavn@gmail.com>
2020-12-06 00:12:59 -0500

    initial commit for news

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+            <div class="container-fluid">
+            </div>
+      <api-component></api-component>
+
+  </div>
+</template>
+
+<script>
+import ApiComponent from "./components/ApiComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    ApiComponent,
+  },
+};
+</script>
+
